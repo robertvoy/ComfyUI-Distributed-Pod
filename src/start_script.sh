f504886eff52920e66e8fb9c5bbe1f3e9f351dd3
robertvoy <hello@robertvoy.com>
2025-07-28 08:43:03 +1000

    start_script fix

--- src/start_script.sh
+++ src/start_script.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Remove existing directory if it exists, then clone fresh
+rm -rf ComfyUI-Distributed-Pod
 git clone https://github.com/robertvoy/ComfyUI-Distributed-Pod.git
-mv ComfyUI-Distributed-Pod/src/start.sh /
+mv ComfyUI-Distributed-Pod/src/start.sh /start.sh
+chmod +x /start.sh
 bash /start.sh
